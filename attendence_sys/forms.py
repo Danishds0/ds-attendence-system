eecaab9dd5b9952e39ebf05822072b88f349be36
Danishds0 <danishshaikhds085@gmail.com>
2022-05-25 17:37:04 +0530

    initialsetup

---
+++ attendence_sys/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import *
+
+class CreateStudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super(CreateStudentForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+    
+class FacultyForm(ModelForm):
+    class Meta:
+        model = Faculty
+        fields = '__all__'
+        exclude = ['user']
+    def __init__(self, *args, **kwargs):
+        super(FacultyForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'    
+
+class CreateUserForms(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1','password2']
